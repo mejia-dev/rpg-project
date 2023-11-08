9a97fc8dc104bb0294fe9fcb7a90d57c61a5f03d
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-08 11:58:51 -0800

    Pass first test for game.js
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>

---
+++ __tests__/game.test.js
@@ -0,0 +1,21 @@
+import { Game } from "../src/js/game.js";
+
+describe('Game', () => {
+
+  test('should create game object from game constructor', () => {
+    let game1 = new Game();
+    // expect(game1.move).toEqual(game.move());
+    expect(game1.mapPosition).toEqual(0);
+    expect(game1.characters).toEqual({});
+    expect(game1.enemy).toEqual({});
+  });
+
+  test('to see if move function properly rolls random', () => {
+    let game1 = new Game();
+    expect(game1.move()).toBeLessThan(4);
+    expect(game1.move()).toGreaterThan(0);
+  });
+})
+
+// expect(diceRollTotal).toBeLessThan(7);
+// expect(diceRollTotal).toBeGreaterThan(0);
---
+++ src/js/game.js
@@ -0,0 +1,55 @@
+export class Game {
+  constructor() {
+    // this.move = move;
+    this.mapPosition = 0;
+    this.characters = {};
+    this.enemy = {};
+    }
+
+
+
+// AddressBook.prototype.addContact = function(c[ontact]) {
+//   contact.id = this.assignId();
+//   this.contacts[contact.id] = contact;
+// };
+// Math.floor(Math.random(1) * 3)
+
+// addCharacter(character) {
+//   this.characters[1] = character;
+// }
+
+  move() {
+    if (submit = "buttonMove") {
+      let currentMove = Math.floor(Math.random(1) * 3);
+    } return currentMove;
+  }  
+  
+  //   mapPosition += currentMove;
+  // } if (mapPosition = 3) {
+  //   this.battle()
+  // } else if (mapPosition = 6) {
+  //   this.battle()
+  // } else if (mapPosition = 9) {
+  //   this.battle()
+  // } else if (mapPosition = 10) {
+  //   youWin()
+  // } else {
+  //   return this.mapPosition;
+  // }
+  // return this.mapPosition;
+  // }
+  
+  // battle() {
+    //battle
+//     returns character.health
+//   }
+// }
+//Game Play constructor
+//map variable
+// if mapPostition var = map @ diff spots triggers events
+//class mapPostionstarts at 0,
+// going yo have function() that increases map position 
+//based on Math.random roll
+
+
+}
