568d36a04c2a163bda9293047e0598ee55a5cadc
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-09 10:32:11 -0800

    Comment out sample code
    Co-authored-by: Monica Barboza <MonBoza@users.noreply.github.com>
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import OtherModule from './js/otherModule.js';
 
-function handleSampleForm() {
-  event.preventDefault();
-  document.getElementById("outputDiv").innerText = null;
-  const pTag = document.createElement("p");
-  pTag.append(document.getElementById("text-input").value);
-  document.getElementById("outputDiv").append(pTag);
-}
+// function handleSampleForm() {
+//   event.preventDefault();
+//   document.getElementById("outputDiv").innerText = null;
+//   const pTag = document.createElement("p");
+//   pTag.append(document.getElementById("text-input").value);
+//   document.getElementById("outputDiv").append(pTag);
+// }
 
-window.addEventListener("load", function() {
-  document.getElementById("sample-form").addEventListener("submit", handleTriangleForm);
-});
+// window.addEventListener("load", function() {
+//   document.getElementById("sample-form").addEventListener("submit", handleTriangleForm);
+// });
